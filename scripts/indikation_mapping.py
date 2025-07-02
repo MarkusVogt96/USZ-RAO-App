7411402d4bfa82b0ea3f8ad70b98d76c0aaaef18
MarkusVogt96 <markusvogt1996@gmail.com>
2025-07-02 10:28:17 +0200

    Update Indikation mapping

---
+++ scripts/indikation_mapping.py
@@ -0,0 +1,155 @@
+C79.5, C78.0, C78.7, C79.86, C79.9: 
+Oligometastasierung:
+Metachrone Oligometastasierung
+Die randomisierte SABR-COMET Studie zeigte, dass bei Patienten mit kontrolliertem Primärtumor und bis zu 5 Fernmetastasen die stereotaktische Radiotherapie (SBRT) aller Metastasen zusätzlich zur standard of care Systemtherapie zu einem klinisch bedeutsamen und statistisch signifikanten Überlebensvorteil mit einem Hazard Ratio von 0.47 führte.
+Palma, D. A. et al. (2020) ‘Stereotactic Ablative Radiotherapy for the Comprehensive Treatment of Oligometastatic Cancers: Long-Term Results of the SABR-COMET Phase II Randomized Trial’, Journal of clinical oncology: official journal of the American Society of Clinical Oncology, p. JCO2000818.
+    
+    Synchrone Oligometastasierung
+Die randomisierten Studien von Gomez et al. bzw Iyengar et al. zeigten, dass Patienten mit nicht-kleinzelligem Bronchialkarzinom (NSCLC) und maximal 3 bis 5 Fernmetastasen, die nach Erstlinien Systemtherapie eine stabile Erkrankung oder eine partielle Remission zeigten, durch eine konsolidierende stereotaktische Radiotherapie (SBRT) aller Metastasen zusätzlich zur standard of care Erhaltungstherapie ein signifikant längeres progressionsfrei Überleben und Gesamtüberleben haben.
+Gomez, D. R. et al. (2020) ‘Local consolidative therapy versus maintenance therapy or observation for patients with oligometastatic non-small-cell lung cancer without progression after first-line systemic therapy: long-term results of a multiinstitutional, phase II, randomised study’, Journal of Clinical Oncology 37, no. 18 1558-1565.
+Iyengar, P. et al. (2018) ‘Consolidative radiotherapy for limited metastatic non-small-cell lung cancer: A phase 2 randomized clinical trial’, JAMA Oncology, 4(1). doi: 10.1001/jamaoncol.2017.3501.
+Zur Verbesserung der lokalen Kontrolle (inkl. Symptomkontrolle), des
+
+progressionsfreien Überlebens und auch des Gesamtüberlebens besteht somit bei synchroner und metachroner Oligometastasierung die Indikation zur stereotaktischen Strahlentherapie.
+
+Oligoprogression:
+Aufgrund der Prognose (antizipiert > 6 Monate), der kürzeren Therapiedauer, des günstigen Toxizitätsprofils und der höheren biologischen Dosis ist bei Oligoprogression der stereotaktischen Strahlentherapie (SBRT) der Vorzug gegenüber einer palliativ dosierten konventionellen Strahlentherapie zu geben, um eine langfristige lokale Tumorkontrolle und eine Verbesserung des progressionsfreien Überlebens zu ermöglichen.
+Polymetastasierung:
+Für Patienten mit multipler Metastasierung, die eine antizipierte Lebenserwartung von mehr als sechs Monaten haben und eine palliative Strahlentherapie indiziert ist, sollte für eine dauerhafte Symptomkontrolle und längerfristige lokale Kontrolle zur Vermeidung von Komplikationen durch eine lokale Progression die Indikation für eine stereotaktische Strahlentherapie geprüft werden.
+
+
+C34:
+Stereotaxie NSCLC Frühstadium: Beim NSCLC im frühen Stadium stellt die SBRT die kurative Therapie der Wahl dar wenn der Patient aus internistischen Gründen inoperable (Lobektomie) ist oder die Operation ablehnt (NCCN, ESMO).
+
+
+C64:
+Primäre SBRT Nierenzellkarzinom: Mehrere prospektive Phase-II Studien haben ein sehr gutes onkologisches Outcome der Stereotaktischen Bestrahlung des Nierenzellkarzinom gezeigt. Somit ist die SBRT bei Patienten mit lokal begrenztem Nierenzellkarzinom eine kurative Therapieoption.
+Rühle ctRO 2019, Siva BJU Int 2017, Correa Eur Urol Focus 2019
+
+C67:
+Trimodel Therapie Indikation:
+Die trimodale Therapie (TUR-B, Radiotherapie, Chemotherapie), stellt beim lokalisierten, muskelinvasiven Harnblasenkarzinom (cT2-4 cN0 cM0) die einzige organerhaltende, kurative Therapieoption und eine Alternative zur radikalen Zystektomie dar.
+Hierbei ist die kombinierte Radiochemotherapie gegenüber der alleinigen Radiotherapie bezüglich lokaler Kontrolle überlegen. Somit ist für das muskelinvasive Harnblasenkarzinom die Radiochemotherapie nach TURB gemäss S3- und NCCN-Leitlinie eine kurative Therapiemöglichkeit bei inoperablen PatientInnen sowie bei Wunsch nach Organerhalt.
+NCCN, S3, James 2012, Hall 2017, Coppin 1996
+
+C24:
+RT neoadjuvant vor geplanter Transplantation beim nicht resektablen perihilären CCC : Mayo-Protokoll
+Beim nicht resektablen perihilären CCC, < 3 cm Grösse und ohne Nachweis von LK-Befall, kann die Lebertransplantation evaluiert werden. Dabei konnte mittels neoadjuvanter Radiochemotherapie (Capecitabine, dieses wird auch nach der RCHT fortgeführt) eine deutliche Verbesserung des Überlebens dieser Patienten erzielt werden, weshalb die neoadjuvante RCHT ein elementarer Bestandteil dieses Therapie-Konzeptes ist.
+Rosen et al, 2010, Tan et al, 2020 (Update)
+
+
+Nicht-resektables CCC:
+Beim nicht-resektablen Cholangiokarzinom kann bei Patienten, die auf eine initiale systemische Therapie über mehrere Monate, nicht progredient sind, eine Lokaltherapie zur Verbesserung der Lokalkontrolle evaluiert werden. Die Radiotherapie konnte dabei in einer kürzlich erschienen Systematischen Übersichtsarbeit eine gepoolte 1-Jahres-Lokalkontrollraten von 79% erzielen.
+Das Fraktionierungsschema sollte dabei in Abhängigkeit der Lage und Grösse gewählt werden.
+NCCN-Guidelines Hepatobiliary 2020, Lee al, 2019
+
+
+Adjuvante Radio(chemo)Therapie nach Resektion:
+Insgesamt ist die Datenlage zur adjuvanten Radiochemotherapie beim resezierten CCC inkonsistent. In Abwesenheit von Phase-III-Studien ist die Radiochemotherapie als mögliche therapeutische Alternative zur adjuvanten Systemtherapie oder Anschlussbehandlung nach Chemotherapie bei R1-Resektions- oder positivem LK-Status erwähnt.
+NCCN-Guidelines Hepatobiliary 2020
+
+
+Palliativ:
+Eine palliative Bestrahlung von Cholangiozellulären Karzinomen kann zur Symptomkontrolle indiziert werden.
+NCCN-Guidelines Hepatobiliary 2020
+
+C22:
+SBRT als Lokaltherapie:
+Bei Patienten mit HCC, die nicht für eine Leber-Transplantation oder Resektion qualifizieren, sollte eine Lokaltherapie diskutiert werden. Dabei ist die SBRT in Guidelines als Alternative zu anderen Lokalverfahren (Ablation, Embolisation) erwähnt, insbesondere, wenn aufgrund der Tumorgrösse (> 3 cm) oder Lokalisation mit Nähe zu grossen Gefässen oder Gallenwegen das Lokalrezidiv-Risiko von Thermal-Ablativen Verfahren hoch ist. In einer Metaanalyse konnte mittels SBRT eine gepoolte Lokalkontrollrate von 84% nach 2 Jahren gezeigt werden.
+(NCCN-Guidelines Hepatobiliary 2020, ESMO Guidelines 2019, Rim et al, 2019)
+Eine Lokaltherapie kann auch als Bridging-Verfahren eingesetzt werden mit dem Ziel die Drop-out Rate von der Transplantations-Liste aufgrund Tumorprogress zu verringern. Es gibt keine prospektiven Studien, die verschiedene Lokaltherapie-Verfahren miteinander vergleichen. In einer retrospektiven Analyse war die SBRT zu anderen Lokalverfahren (RFA, TACE) gleichwertig hinsichtlich Rate an tatsächlich erfolgter Leber-Transplantation, postoperativen Komplikationen oder Gesamt-Überleben obschon Patienten, die der SBRT zugeführt wurden, aufgrund der verminderten Leberfunktion keine Kandidaten für ein anderes Therapie-Verfahren waren.
+(Sapisochin et al, 2017)
+Bei fortgeschrittenen HCC ohne Möglichkeit einer Resektion oder Transplantation kann eine Radiotherapie, kombiniert zu einer TACE evaluiert werden. In einer kleinen randomisieren Studie war dies der alleinigen Sorafenib Therapie überlegen.
+(Yoon et al, 2018)
+
+RT neoadjuvant vor Resektion:
+Vaskuläre Thromben sind als Risikofaktor für ein schlechteres Outcome bei HCC bekannt. Bei resezierbaren HCC-Tumoren mit Vorliegen eines Portalvenen-Thrombus konnte durch eine neoadjuvante Bestrahlung ein besseres krankheits-freies und Gesamtüberleben gezeigt werden.
+(Wei et al, 2019).
+
+Palliativ:
+Eine palliative Bestrahlung von Lebertumoren kann zur Symptomkontrolle indiziert werden.
+
+
+C15:
+Ösophagus Primär-definitive Radiochemotherapie:
+Bei lokal fortgeschrittenen anatomisch oder funktionell inoperablen Tumoren stellt die Radiochemotherapie die einzige kurative Therapieoption dar. Bei operablen Tumoren verbessert die zusätzliche Operation nach Radiochemotherapie die lokale Kontrolle, jedoch nicht das Gesamtüberleben, so dass eine definitive Radiochemotherapie, je nach Risikoprofil, eine geeignete Therapiealternative bietet und gemäss S3-Leitlinie eine Standardtherapie darstellt.
+S3-Leitlinie V2.01-10/2018, Stahl M. J Clin Oncol. 2005, Bedenne et al, J Clin Oncol. 2007
+Ab Stadium IIB als Alternative zur Operation generell zu diskutieren, insbesondere bei Plattenepithelkarzinomen des oberen und mittleren Drittels (Patientenwunsch/OP-Morbidität abzuwägen).
+Bei inoperablen Tumoren RCT als einzige weitere kurative Option (Pöttgen et al Cancer Treat Rev. 2012).
+
+Ösophagus neoadjuvante Radiochemotherapie:
+Bei lokal fortgeschrittenen operablen Tumoren des Oesophagus und des oesophagogastralen Überganges ab St. IIB verbessert eine präoperative Radiochemotherapie hochsignifikant mit einer HR von 0.657 das Gesamtüberleben unabhängig vom histologischen Subtyp gegenüber einer alleinigen Operation. Somit ist beim lokal fortgeschrittenen operablen Oesophaguskarzinom die neoadjuvante Radiochemotherapie vor Operation gemäss S3-Leitlinie die Standardtherapie.
+Van Hagen et al., CROSS Trial, NEJM. 2012/2015, S3-Leitlinie V2.01-10/2018
+
+Ösophagus palliative Brachytherapie:
+Ösophageale palliative Brachytherapie ist eine Strahlentherapietechnik, mit der der Tumor mit einer hohen Strahlendosis bestrahlt wird. Dabei bleibt bei einer hohen Oberflächendosis die Dosis auf das umliegende Gewebe relativ gering. Das Ziel der palliativen Brachytherapie ist Linderung der Dysphagie.
+Eine Meta-Analyse zeigte ein DyFS (dysphagia-free survival) von 67% nach 3 Monaten, 47% nach 6 Monaten, und 29% nach 12 Monaten. Ca. 12% den Patienten entwickeln eine Brachytherapiebedingte Stenose und 8% eine Fistel entwickelt (Fucio 2017). Eine Literaturübersicht berichtet von einem mittleren DyFS von 99 Tagen (Lancellotta 2020).
+
+
+C20:
+Beim Rektumkarzinom des unteren und mittleren Drittels senkt ab UICC Stadium II die zusätzliche Radiochemotherapie die Lokalrezidivrate signifikant und erhöht für Tumore des unteren Rektumdrittels die Rate an Sphinktererhalt. Die neoadjuvante Radiochemotherapie ist bei verbesserter lokaler Kontrolle und geringerer Toxizität gemäss des CAO/ARO/AIO-94 einer postoperativen Radiochemotherapie überlegen. Somit ist beim lokal fortgeschrittenen Rektumkarzinom, insbesondere des unteren Drittels die neoadjuvante Radiochemotherapie vor Operation gemäss S3-Leitlinie die Standardtherapie.
+Sauer 2004, Sauer et al, 2012, S3-Leitlinie
+
+Bei lokal fortgeschrittenen operablen Tumoren des Rektums verbessert die Vorbehandlung mit einer kurzzeitigen Radiotherapie die Lokalrezidivrate. Somit ist beim lokal fortgeschrittenen Rektumkarzinom eine neoadjuvante Radiotherapie vor Operation gemäss S3-Leitlinie eine Standardtherapie. Eine Kombination mit einer konsolidierenden Chemotherapie ist möglich.
+Folkesson et al 2005, Stockholm III, Bujko 2016
+
+C25:
+SBRT bei Pankreaskarzinom:
+Bei inoperablen lokal fortgeschrittenen Pankreaskarzinomen stellt die Ablative Bestrahlung in Form einer SBRT eine gute Therapeutische Option mit hohen lokalen Kontrollraten dar (1-Jahres-Kontrollrate von 72.3 % in einer gepoolten systematischen Analyse). Diese Therapie sollte daher allen Patienten mit inoperablem Pankreaskarzinom, welches auf eine initiale mindestens 4-6-monatige Systemtherapie nicht progredient ist, angeboten werden.
+Systematic Review, Petrelli et al., 2017, NCCN Guidelines, ASTRO Guidelines
+
+Beim symptomatischem Pankreaskarzinom kann eine lokale Bestrahlung des Primärtumors zur Linderung der Beschwerden führen. Dabei kann eine moderate Dosisintensivierung mittels SBRT im Rahmen der offenen klinischen Studie durchgeführt werden (MASPAC trial).
+
+
+
+
+Kombinierte Radiochemotherapie bei lokal fortgeschritten inoperabel (LAPC):
+Bei inoperablen lokal fortgeschrittenen Tumoren führt eine Bestrahlung zu einer Verbesserung der lokalen Tumorkontrolle. Falls eine SBRT nicht möglich ist, kann in diesem Fall bei Patienten mit inoperablem Pankreaskarzinom, welches auf eine initiale mindestens 4-6-monatige Systemtherapie nicht progredient ist, eine konventionell fraktionierte Radiotherapie, begleitet von einer Mono-Chemotherapie, angeboten werden.
+Hammel et al., 2016 (LAP-07), Loehrer et al, 2011 (ECOG 4201), Chen et al, 2013, Metaanalyse
+
+
+Kombinierte Radiochemotherapie Adjuvant: (Im Einzelfall diskutable Indikation)
+Eine adjuvante Radiochemotherapie kann bei hohem Lokalrezidiv-Risiko (R1) indiziert werden, falls mFOLFIRINOX nicht gegeben werden kann.
+Morganti et al, 2014, Butturini et al, 2008
+
+C21:
+Bei Analkarzinomen stellt die Radiochemotherapie die einzige kurative Therapieoption dar und ist somit bei allen Tumoren >T1N0, modifiziert nach RTOG/ECOG-Protokoll, die Standardtherapie (Flam et al., JCO 1996). Diese ist einer alleinigen Radiotherapie überlegen (EORTC-trial, Bartelink et al. JCO 1997). Auch im Vergleich zu operativen Massnahmen ist die lokale Kontrolle mit Radiochemotherapie überlegen und auch bei fortgeschrittenen Stadien noch sehr hoch (Grabenbauer DisColRectum 2005). Somit ist für das Analkarzinom die Radiochemotherapie gemäss NCCN-Leitlinie die Standardtherapie.
+Flam 1996, Bartelink 1997, Grabenbauer 2005
+
+
+C34:
+SBRT Radiotherapie bei NSCLC Stadium I-IIb (cN0):
+Für das nicht-kleinzellige Lungenkarzinom im Stadium I und II ohne Lymphknotenbefall ist die stereotaktische Strahlentherapie für inoperable Patienten aufgrund der geringen Toxizität, der sehr hohen lokalen Kontrolle von über 90% und der kurzen Behandlungszeit die Therapie der Wahl.
+NCCN guideline, Postmus PE et al. Ann Oncol 2017, Schneider BJ et al. JCO 2017, Guckenberger M et al. Radiother Oncol 2017, Videtic GMM et al. PRO 2017, Ball D et al. Lancet Oncol 2019
+
+
+Radiotherapie bei NSCLC Stadium III:
+Die simultane Radiochemotherapie ist gemäss der Metaanalyse von Aupérin et al. 2018 einer sequentiellen oder alleinigen Radiotherapie bezüglich des Gesamtüberlebens mit einer Hazard Ratio von 0.84 überlegen. Sie ist somit im Stadium III mit kurativer Intention gemäss NCCN- und ESMO-Leitlinie als der Standard für die Lokaltherapie anzusehen.
+NCCN guideline, Postmus PE et al. Ann Oncol 2017, Auperin A et al. JCO 2010
+Aufgrund der zusätzlichen Verbesserung des Gesamtüberlebens durch die Reduktion einer späteren Fernmetastasierung sowie einer Verbesserung der lokalen Tumorkontrolle sollte eine adjuvante Immuntherapie mit Durvalumab nach Abschluss der Radiochemotherapie erfolgen.
+Antonia SJ et al. NEJM 2018
+
+
+Radiotherapie bei SCLC limited disease:
+ Beim SCLC im Stadium limited disease verbessert die kombinierte Radiochemotherapie gefolgt von einer PCI das Gesamtüberleben. Daher stellt dieses Behandlungskonzept den Behandlungsstandard dar.
+S3 Leitlinie, NCCN guideline, Murray N et al. JCO 1993, Auperin A et al. NEJM 1999
+
+Radiotherapie bei SCLC extensive disease:
+- Beim SCLC im Stadium extensive disease verbessern die konsolidierende thorakale Radiotherapie und PCI das Gesamtüberleben bei Patienten, welche auf die palliative Systemtherapie angesprochen haben. (NCCN Guidelines)
+- Bei guter PR konsolidierende thorakale RTx. (Slotman BJ et al. Lung Cancer 2017)
+- Bei CR unter CTx und Atezolizumab kann auf die thorakale RTx verzichtet werden. (Horn 2018)
+- Bei mediastinaler Oligoprogression thorakale Radiotherapie zur Verbesserung der lokalen Kontrolle. (Paz-Ares 2019)
+
+C45:
+Malignes Pleuramesotheliom:
+Die adjuvante Radiotherapie des Mesothelioms nach Pleurektomie/ Dekortikation/extrapleurale Pneumektomie bei Stadium I-IIIA kann eine effektive Therapieoption zur Verbesserung der lokalen Kontrolle bei einer R1-Situation darstellen, bei Patienten mit einem ECOG ≤1, guter Lungenfunktion sowie keine Anwesenheit von extrathorakalen Läsionen, wenn in einem interdisziplinären Tumorboard die Indikation für eine Lokaltherapie zur Verbesserung der lokalen Tumorkontrolle gestellt wurde.
+(IMPRINT JCO 2016, Rusch J Thorac Cardiovasc Surg 2001)
+
+Die palliative Radiotherapie des Mesothelioms ist eine effektive Therapieoption bei Schmerzen, bronchiale/oesophageale Obstruktion oder extrathorakale Metastasen.
+NCCN Guidelines, ASCO Guidelines, SYSTEMS-1 Trial, Macleod Lung Cancer 2015
+
+Die stereotaktische Radiotherapie des Mesothelioms kann bei Oligorecurrence oder Oligoprogression eine effektive Massnahme darstellen, wenn in einem interdisziplinären Tumorboard die Indikation für eine Lokaltherapie zur Verbesserung des progressionsfreien Überlebens sowie der lokalen Tumorkontrolle gestellt wurde.
+Schröder Front Oncol 2019
+
+
+
